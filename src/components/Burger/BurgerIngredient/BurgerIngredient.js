6049dbf56760bcdeca738c93495e83d8bd5a1632
kritsanai <kritsanai@hotmail.com>
2018-07-23 23:05:46 +0700

    ddd

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './BurgerIngredient.css'
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+
+    switch(props.type) {
+        case ('bread-buttom') : 
+            ingredient = <div className={classes.BreadBottom}></div>
+            break;
+    }
+}
+
+export default burgerIngredient;
